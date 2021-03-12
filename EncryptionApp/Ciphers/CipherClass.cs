b9cdd2a90548fa8d6aa9ba21ba7cb256ee92a9d2
psp515 <kolberu@gmail.com>
2021-03-12 19:27:43 +0100

    01

---
+++ EncryptionApp/Ciphers/CipherClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncryptionApp.Ciphers
+{
+    public interface CipherClass
+    {
+       
+        public virtual string Encode(string A) { return A; }
+        public virtual string Decode(string A) { return A; }
+        
+
+    }
+}
